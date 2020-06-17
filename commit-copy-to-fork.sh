a98e0be644fe88a1859e92a789d25bbbf5e8c2f7
Hans5958 <email5958.1@gmail.com>
2020-06-17 22:09:39 +0700

    Add new script

---
+++ commit-copy-to-fork.sh
@@ -0,0 +1,19 @@
+letter_folder=${1::1}
+letter_folder=${letter_folder^^}
+echo "Preparing repository branch..."
+git config --global user.email "email5958.1@gmail.com"
+git config --global user.name "Hans5958"
+ver=$(sed -e 's/^"//' -e 's/"$//' <<< $(jq .version "$1/dist/metadata.json"))
+cd ../PreMiD-Presences
+cd ../PreMiD-Presences-Personal
+echo "Copying..."
+bash copy-to-fork.sh "$1"
+cd ../PreMiD-Presences
+echo "Creating commit..."
+git add "websites/$letter_folder/$1/*"
+if [[ $ver == "1.0.0" ]]; then 
+    git commit --message "Add $1 (1.0.0)"
+else
+    git commit --message "Update $1 ($ver)"
+fi
+echo "All done!"
