46b30c6c89a33c63015d7ec4b76b730fc05ca1c1
Hans5958 <email5958.1@gmail.com>
2020-06-17 20:33:40 +0700

    Script update for names with spaces

--- branch-copy-to-fork.sh
+++ branch-copy-to-fork.sh
@@ -3,16 +3,17 @@ letter_folder=${letter_folder^^}
 echo "Preparing repository branch..."
 git config --global user.email "email5958.1@gmail.com"
 git config --global user.name "Hans5958"
-ver=$(sed -e 's/^"//' -e 's/"$//' <<< $(jq .version $1/dist/metadata.json))
+ver=$(sed -e 's/^"//' -e 's/"$//' <<< $(jq .version "$1/dist/metadata.json"))
 branch=$(echo "${1,,}")-$ver
+branch=${branch// /-}
 cd ../PreMiD-Presences
 git checkout master
-git branch -D $branch
-git checkout -b $branch
+git branch -D "$branch"
+git checkout -b "$branch"
 # git status
 cd ../PreMiD-Presences-Personal
 echo "Copying..."
-bash copy-to-fork.sh $1
+bash copy-to-fork.sh "$1"
 cd ../PreMiD-Presences
 echo "Creating commit..."
 git add "websites/$letter_folder/$1/*"
