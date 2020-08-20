430b0ea6ed273933433160b9cb42f8ee95ddfcc9
Hans5958 <email5958.1@gmail.com>
2020-08-20 20:24:16 +0700

    PW: Add batch compile script
    #3

---
+++ pw-batch-compile.bat
@@ -0,0 +1,20 @@
+REM tsc.cmd -p "Wikibooks\tsconfig.json"
+REM tsc.cmd -p "Wikidata\tsconfig.json"
+REM tsc.cmd -p "Wikimedia Commons\tsconfig.json"
+REM tsc.cmd -p "Wikinews\tsconfig.json"
+REM tsc.cmd -p "Wikiquote\tsconfig.json"
+REM tsc.cmd -p "Wikisource\tsconfig.json"
+REM tsc.cmd -p "Wikispecies\tsconfig.json"
+REM tsc.cmd -p "Wikiversity\tsconfig.json"
+REM tsc.cmd -p "Wikivoyage\tsconfig.json"
+REM tsc.cmd -p "Wiktionary\tsconfig.json"
+branch-copy-to-fork.sh Wikibooks
+branch-copy-to-fork.sh Wikidata
+branch-copy-to-fork.sh Wikimedia Commons
+branch-copy-to-fork.sh Wikinews
+branch-copy-to-fork.sh Wikiquote
+branch-copy-to-fork.sh Wikisource
+branch-copy-to-fork.sh Wikispecies
+branch-copy-to-fork.sh Wikiversity
+branch-copy-to-fork.sh Wikivoyage
+branch-copy-to-fork.sh Wiktionary
